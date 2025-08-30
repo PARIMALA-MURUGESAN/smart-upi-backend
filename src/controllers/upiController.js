df0d92d65850ea3c43d776fa4707815d0bcc78de
Parimala Murugesan <parimalamurugesan2007@gmail.com>
2025-08-30 08:26:14 +0530

    Fix undefined getQr by implementing QR generator

--- src/controllers/upiController.js
+++ src/controllers/upiController.js
@@ -1,8 +1,27 @@
 const User = require('../models/User');
 const { generateUpiQr } = require('../services/qrService');
-
 const { decrypt } = require('../services/enc');
 const History = require('../models/History');
+
+async function getQr(req, res) {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).send('User not found');
+
+    const preferred = user.upis.id(user.preference.preferredUpiId);
+    if (!preferred) return res.status(400).send('No preferred UPI set');
+
+    const vpa = decrypt(preferred.vpa_encrypted);
+    const qrImage = await generateUpiQr(vpa, user.name);
+
+    res.json({ qrImage });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to generate QR');
+  }
+}
+
 async function handlePaymentLink(req, res) {
   try {
     const { userId } = req.params;
@@ -18,9 +37,9 @@ async function handlePaymentLink(req, res) {
     // redirect to UPI deep link
     res.redirect(upiLink);
   } catch (err) {
+    console.error(err);
     res.status(500).send('Failed to process payment link');
   }
 }
-
 
 module.exports = { getQr, handlePaymentLink };
