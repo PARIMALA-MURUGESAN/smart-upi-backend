6a2cd45145c11915ac1bb17ce34643593231c58f
Parimala Murugesan <parimalamurugesan2007@gmail.com>
2025-09-10 22:17:06 +0530

    Fix update server.js

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const express = require("express");
+const authRoutes = require("./src/routes/authRoutes");  // 👈 make sure path is correct
+const paymentRoutes = require("./src/routes/paymentRoutes");
+const upiRoutes = require("./src/routes/upiRoutes");
+const preferenceRoutes = require("./src/routes/preferenceRoutes");
 
 dotenv.config();
 
-const app = express();
+const app=express()
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);         // 👈 register/login
+app.use("/api/payments", paymentRoutes);  // 👈 send money
+app.use("/api/upis", upiRoutes);          // 👈 manage UPIs
+app.use("/api/preferences", preferenceRoutes); // 👈 auto-switch rules
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("✅ Smart UPI Backend Running");
+});
+
+
+
 
 const PORT = process.env.PORT || 5000;
 const DB_URI = process.env.DB_URI;
