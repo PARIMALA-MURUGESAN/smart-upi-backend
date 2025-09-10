92cd5fbe01e530820619031fe03a203bee438159
Parimala Murugesan <parimalamurugesan2007@gmail.com>
2025-09-10 14:49:36 +0530

    Fix update paymentRoutes

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { sendPayment } = require("../controllers/paymentController");
+const { sendMoney } = require("../controllers/paymentController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 // POST /api/payments/send
-router.post("/send", authMiddleware, sendPayment);
+router.post("/send", authMiddleware, sendMoney);
 
 module.exports = router;
