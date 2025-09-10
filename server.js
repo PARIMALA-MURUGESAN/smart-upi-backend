70b584fc9a91c24eba769247cec30f8e124ec40f
Parimala Murugesan <parimalamurugesan2007@gmail.com>
2025-09-10 17:36:42 +0530

    Fix update server.js

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+const DB_URI = process.env.DB_URI;
 
-if (!MONGO_URI) {
+if (!DB_URI) {
   console.error("❌ MONGO_URI is missing in .env file");
   process.exit(1);
 }
 
-mongoose.connect(MONGO_URI)
+mongoose.connect(DB_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch(err => console.error("❌ MongoDB Connection Error:", err.message));
 
